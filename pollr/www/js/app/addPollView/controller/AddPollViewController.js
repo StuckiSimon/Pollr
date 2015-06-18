3ed2e35b79891f0fd753d760db1f228e7f198b01
Simon Stucki <simon_stucki@hotmail.com>
2015-06-18 16:30:50 +0200

    AddPollView Client Side Implementation
    still missing post on server

--- pollr/www/js/app/addPollView/controller/AddPollViewController.js
+++ pollr/www/js/app/addPollView/controller/AddPollViewController.js
@@ -2,6 +2,15 @@ define(['addPollView/namespace', 'shared/webServiceManager/namespace'], function
   return function(module) {
     module.controller(namespace + ".addPollViewController", ['$scope', '$ionicModal', webServiceManagerNamespace + '.pollManagementService', function($scope, $ionicModal, pollManagementService) {
 
+      $scope.pollData = {
+        title: "",
+        options: []
+      };
+
+      $scope.dialogData = {
+        newOption: "",
+      };
+
       var initModal = function() {
         $ionicModal.fromTemplateUrl('js/app/addPollView/template/addChangeOptionDialog.html', function(modal) {
           $scope.addDialog = modal;
@@ -20,6 +29,40 @@ define(['addPollView/namespace', 'shared/webServiceManager/namespace'], function
       $scope.leaveAddChangeOptionDialog = function() {
         $scope.addDialog.remove();
         initModal();
+      };
+
+      $scope.addOptionToPollData = function() {
+        if ($scope.dialogData.newOption.length === 0) {
+          return;
+        }
+        $scope.pollData.options.push({
+          title: $scope.dialogData.newOption
+        });
+        $scope.dialogData.newOption = "";
+        $scope.leaveAddChangeOptionDialog();
+      };
+
+      $scope.newOptionIsValid = function() {
+        var option = $scope.dialogData.newOption,
+          options = $scope.pollData.options;
+        if (option.length > 0) {
+          for (i in options) {
+            if (option === options[i].title) {
+              return false;
+            }
+          }
+          return true;
+        }
+        return false;
+      };
+
+      $scope.newPollIsValid = function() {
+        var options = $scope.pollData.options,
+          title = $scope.pollData.title;
+        if (title.length > 0 && options.length > 1) {
+          return true;
+        }
+        return false;
       };
 
     }]);
