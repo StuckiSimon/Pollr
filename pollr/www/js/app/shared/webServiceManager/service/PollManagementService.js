482b458d3be6e79ca1b3dee4901a395cbc409f07
Simon Stucki <simon_stucki@hotmail.com>
2015-06-18 16:00:39 +0200

    remove sass stylesheet from index.html
    browser environments aren't able to interpret SASS

--- pollr/www/js/app/shared/webServiceManager/service/PollManagementService.js
+++ pollr/www/js/app/shared/webServiceManager/service/PollManagementService.js
@@ -3,7 +3,9 @@ define(['shared/webServiceManager/namespace'], function(namespace) {
     module.factory(namespace + ".pollManagementService", ['$http', function($http) {
       return {
         loadPosts: function() {
-          return $http.get('http://pollr.lamatouren.ch/pollr/polls');
+          return $http.get('http://pollr.lamatouren.ch/pollr/polls').success(function(data) {
+            console.log(data);
+          });
         },
         getPollOptions: function(pollId) {
           return $http.get('http://pollr.lamatouren.ch/pollr/pollOptions?pollId=' + pollId);
